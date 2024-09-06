3d8968167ffa29d439c70ae0432dbf49ab4f2a80
patrick <patricklauxx@gmail.com>
2024-09-07 06:27:37 +0800

    init

---
+++ xcache-samples-base/src/main/java/com/igeeksky/xcache/Main.java
@@ -0,0 +1,14 @@
+package com.igeeksky.xcache;
+
+/**
+ * @author Patrick.Lau
+ * @since 1.0.0 2024/9/7
+ */// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+    }
+}
